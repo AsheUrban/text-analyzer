8879112e762e6484c7d7c4e5a8fb4acccd26ba06
Ashe Urban <ashe@goldentongue.com>
2022-06-07 17:31:28 -0700

    FIX bug for number of occurences and run test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,48 @@
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
-}
+  }
   let wordCount = 0;
   const wordArray = text.split(" ");
   wordArray.forEach(function(element) {
     if (!Number(element)) {
-    wordCount++;
+      wordCount++;
     }
   });
   return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
     return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+      wordCount++;
     }
   });
   return wordCount;
 }
 
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
+
+// Business logic we want to save!!!
+
+// From Zachary!
 // function removeBadWords(words,text) {
 //   const noBadWords = text.split(" ");
 //   let i = 0;
@@ -40,17 +57,18 @@ function numberOfOccurrencesInText(word, text) {
 //   return noBadWords;
 // }
 
-function omitOffensiveWords(word, text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let goodtext = []
-  let textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (!offensiveWords.includes(element.toLowerCase())) {
-      goodtext.push(element);
-    }
-  });
+// From Max!
+// function omitOffensiveWords(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let goodtext = []
+//   let textArray = text.split(" ");
+//   textArray.forEach(function(element) {
+//     if (!offensiveWords.includes(element.toLowerCase())) {
+//       goodtext.push(element);
+//     }
+//   });
 
-  return goodtext;
-}
+//   return goodtext;
+// }
