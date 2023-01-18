85a7760c72c1db08e384100f7cbb1d88de74d213
Ashe Urban <ashe@goldentongue.com>
2023-01-18 13:49:39 -0800

    Refactor with isEmpty function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return testString.trim().length === 0;
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
 
@@ -16,7 +22,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (word.trim().length === 0) {
+  if (isEmpty(word)) {
     return 0;
   }
   const textArray = text.split(" ");
@@ -32,27 +38,22 @@ function numberOfOccurrencesInText(word, text) {
 // This project contains two functions that effectively remove bad words from text. One uses a boolean, the other uses comparative operators.
 
 function removeOffensiveWords(text) {
-  const offensiveWords = [
-    "zoinks",
-    "muppeteer",
-    "biffaroni",
-    "loopdaloop"
-  ]
+  const offensiveWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
   const textArray = text.split(" ");
-  let outputArr = []
-  textArray.forEach(function(word) {
+  let outputArr = [];
+  textArray.forEach(function (word) {
     let isOkay = true;
-    offensiveWords.forEach(function(offensiveWord) {
-      if(word.toLowerCase().includes(offensiveWord.toLowerCase())) {
+    offensiveWords.forEach(function (offensiveWord) {
+      if (word.toLowerCase().includes(offensiveWord.toLowerCase())) {
         isOkay = false;
       }
-    })
-    if(isOkay) {
-      outputArr.push(word)
+    });
+    if (isOkay) {
+      outputArr.push(word);
     }
-  })
-  return outputArr.join(" ")
-};
+  });
+  return outputArr.join(" ");
+}
 
 // function removeBadWords(text) {
 //   const badWords = ["zoinks", "shucks", "shoot"];
@@ -61,9 +62,54 @@ function removeOffensiveWords(text) {
 //   let outputArray = [];
 
 //   textArray.forEach(function(word) {
-//     if (!badWords.includes(word.toLowerCase())) 
+//     if (!badWords.includes(word.toLowerCase()))
 //       outputArray.push(word);
 //   })
-  
+
 //   return outputArray.join(" ");
 // };
+
+//User Interface
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function () {
+  document
+    .querySelector("form#word-counter")
+    .addEventListener("submit", handleFormSubmission);
+});
+
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function (element) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== textArray.length - 1) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
